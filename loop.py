f080383f3700e0f445e9fef40f08cce4f88c260d
umar4011 <mursake401@gmail.com>
2025-01-11 13:54:18 +0100

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,7 @@
+# Loops
+x = 10
+y = 1 
+
+while y < x:
+    print("Welcome")
+    y += 1
---
+++ python.py
@@ -0,0 +1,38 @@
+
+# print(dir(names))
+# new_names = ["vincent", "james"]
+# names.append("Mary")
+# names.append(True)
+# names.extend(new_names)
+# names.extend("shegun")
+# names.append(new_names)
+# names.insert(2, "shegun")
+# names.clear()
+# del(names)
+# another_names = names.copy()
+# names = ["John","John", "Paul", "George",]
+# name = "John"
+# count_result = names.count(name)
+# poped = names.pop(2)
+# names.remove("Paul")
+# print(poped)
+# print(names)
+
+# print(another_names)
+
+#       bmw            audi    toyota    benz
+# cars = [["M5", "M6"], "audi", "toyota", ["AMG","GLE"]]
+
+# cars[0][1] = "M1"
+# print(cars)
+
+cars = {
+    "bmw": "M5",
+    "audi": "audi", 
+    "number": 4,
+    "status": True
+}
+# cars["status"] = False
+# our_keys = list(cars.keys())
+our_values = list(cars.values())
+print(our_values)
